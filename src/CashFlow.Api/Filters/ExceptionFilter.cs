6590b262c1a30c3114b301b5568fd4fa3928b6d3
Maria Luiza <marialuizapaes22@gmail.com>
2025-10-08 20:55:53 -0300

    feat - adicionando filtros de excecao e resources

---
+++ src/CashFlow.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,53 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.Api.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if(context.Exception is CashFlowException)
+        {
+            HandleProjectException(context);
+        }
+        else
+        {
+            ThrowUnkowError(context);
+        }
+    }
+
+    private void HandleProjectException(ExceptionContext context)
+    {
+        //verificando o tipo da exceção que ocorreu.
+        if (context.Exception is ErrorOnValidationException)
+        {
+            //Como is apenas verifica, você ainda precisa converter a exceção para o tipo correto.
+            //realizando um cast permitindo acessar propriedades específicas, como ex.Errors
+            var ex = (ErrorOnValidationException)context.Exception;
+
+            var errorResponse = new ResponseErrorJson(ex.Errors);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+        else
+        {
+            var errorResponse = new ResponseErrorJson(context.Exception.Message);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorResponse);
+        }
+    }
+    private void ThrowUnkowError(ExceptionContext context)
+    {
+        var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorResponse);
+    }
+
+}
--- src/CashFlow.Application/UseCases/Expenses/Register/ResgisterExpenseValidator.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/ResgisterExpenseValidator.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Communication.Requests;
+using CashFlow.Exception;
 using FluentValidation;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,9 +7,13 @@ public class ResgisterExpenseValidator : AbstractValidator<RequestResgisterExpen
 {
     public ResgisterExpenseValidator()
     {
-        RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required.");
-        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The Amount must be greater than zero.");
-        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Expenses cannot be for the future.");
-        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("PaymentType is not valid.");
+        //resource usado para evitar a duplicação de código
+        //pode haver mais de uma classe que exiba uma exceção e ai para não duplicar usa o resource
+        //são armazenadas mensagens de erro, textos ou valores reutilizáveis
+        //— em vez de repetir o mesmo texto ou lógica em várias classes.
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage(ResourceErrorMessages.TITLE_REQUIRED);
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage(ResourceErrorMessages.AMOUNT_MUST_BE_GREATER_THAN_ZERO);
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage(ResourceErrorMessages.EXPENSES_CANNOT_BE_FOR_THE_FUTURE);
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage(ResourceErrorMessages.PAYMENT_TYPE_INVALID);
     }
 }
